b4023274e7bfbd457cb6289876a3804a7cbf4334
GUILLEMET Baptiste <baptiste.guillemet@viacesi.fr>
2020-11-23 16:56:29 +0100

    Add methods and attributs FILE + FileState

--- Projet-Sauvegarde/Model/File.cs
+++ Projet-Sauvegarde/Model/File.cs
@@ -4,7 +4,54 @@ using System.Text;
 
 namespace Projet_Sauvegarde.Model
 {
-    class Fikle
+    class File
     {
-    }
-}
+        private String name;
+        private String path;
+        private DateTime creationDate;
+
+        public String name
+        {
+            get { return name; }
+            set { name = value; }
+        
+        }
+
+        public String path
+        {
+            get { return path; }
+            set { path = value; }
+        
+        }
+
+        public DateTime creationDate
+         {
+            get { return creationDate; }
+            set { creationDate = value; }
+        
+        }
+
+
+
+
+        public void SetCreationDate(DateTime : date)
+        {
+
+        }
+
+        public String TransformToJson(String DataToConverted)
+        {
+
+        }
+
+        public DateTime TakeTime()
+        {
+
+        }
+
+
+
+
+    }  //end Class File
+
+} //end namespace
--- Projet-Sauvegarde/Model/StateFile.cs
+++ Projet-Sauvegarde/Model/StateFile.cs
@@ -6,5 +6,43 @@ namespace Projet_Sauvegarde.Model
 {
     class StateFile
     {
-    }
-}
+
+        private String template 
+
+        
+        public String template
+        {
+            get { return template; }
+            set { template = value; }
+        }
+
+        public void ModifyData(String Data)
+        {
+
+        }
+
+        public void SetProgress(int Progress)
+        {
+        
+        }
+
+        public void SetRemainingFile(int RemainingFile)
+        {
+        
+        }
+
+        public void SetRemainingFileSize(int RemainingFileSize)
+        {
+        
+        }
+
+        public void SetState(bool State)
+        {
+        
+        }
+
+
+
+
+    } //end class StateFile
+} //end namespace
